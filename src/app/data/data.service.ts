84beca20491b8e180621ca76955358ed70385f35
Natashi <32347635+Natashi@users.noreply.github.com>
2024-02-27 11:14:02 +0700

    Add documentGetRecents to data service

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -311,6 +311,13 @@ export class DataService {
 		return <Observable<Models.RespDocumentData[]>>
 			this._get(`document/with/account/${accountId}?${query}`);
 	}
+	
+	public documentGetRecents(projectId: number, details: number = 0) {
+		var query = Helpers.bodyToHttpQueryString({},
+			["details", details]);
+		return <Observable<Models.RespDocumentData[]>>
+			this._get(`document/recent/${projectId}?${query}`);
+	}
 
 	public documentUploadToProject(projectId: number, data: Models.ReqBodyUploadDocument) {
 		return <Observable<number>>
