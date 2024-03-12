69f8f240c6579efa6f2acf5bd95bb7ea00ba8144
Natashi <32347635+Natashi@users.noreply.github.com>
2024-03-12 17:47:16 +0700

    Update SubmitQuestionComponent

--- src/app/submit-question/submit-question.component.ts
+++ src/app/submit-question/submit-question.component.ts
@@ -2,8 +2,6 @@ import {
 	Component, OnInit, ChangeDetectorRef,
 	Input, Output,
 } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
 
 import { DataService } from '../data/data.service';
 import { SecurityService } from '../security/security.service';
@@ -12,10 +10,19 @@ import * as Models from "../data/data-models";
 
 import { Helpers } from '../helpers';
 
+class AddQuestionEntry {
+	isAccount: boolean;
+	trancheId?: number;
+	accountId?: string;
+	
+	category: string;
+	text: string;
+}
+
 @Component({
 	selector: 'app-submit-question',
 	templateUrl: './submit-question.component.html',
-	styleUrls: ['./submit-question.component.scss']
+	styleUrls: ['./submit-question.component.scss'],
 })
 export class SubmitQuestionComponent {
 	projectId = 1;
@@ -23,19 +30,40 @@ export class SubmitQuestionComponent {
 	constructor(
 		private dataService: DataService,
 		private securityService: SecurityService,
-		private router: Router,
 	) {
 		this.projectId = securityService.getProjectId();
 	}
 
 	headerLoaded = false;
+	tranchesData: Models.RespTrancheData[] = [];
 	
-	tranchesData: Models.RespTrancheData[];
-
+	addingQuestionsData: AddQuestionEntry[] = [];
+	
+	categorySelectItems = [
+		{
+			label: 'General',
+			id: 'general',
+		},
+		{
+			label: 'Collateral',
+			id: 'collateral',
+		},
+		{
+			label: 'Litigation',
+			id: 'litigation',
+		},
+		{
+			label: 'Guarantor',
+			id: 'guarantor',
+		},
+	];
+	
 	// -----------------------------------------------------
 
 	ngOnInit(): void {
 		this.fetchData();
+		
+		this.addNewQuestion();
 	}
 	
 	async fetchData() {
@@ -54,8 +82,53 @@ export class SubmitQuestionComponent {
 	}
 
 	// -----------------------------------------------------
+	
+	tranchesToSelectItems() {
+		let res =
+			this.tranchesData.map(x => ({
+				label: x.name,
+				id: x.id,
+			}));
+		
+		return res;
+	}
+	getAccountSelectItems(entry: AddQuestionEntry) {
+		let tranche = this.tranchesData
+			.find(x => x.id == entry.trancheId);
+		if (tranche != null) {
+			let res =
+				tranche.accounts.map(x => ({
+					label: x.id,
+					id: x.id,
+				}));
+			
+			return res;
+		}
+		return [];
+	}
+	
+	// -----------------------------------------------------
+	
+	// TODO: Add animations
+	addNewQuestion() {
+		this.addingQuestionsData.push({
+			isAccount: false,
 
-	callbackRadioChange(value: number): void {
-		console.log(value);
+			category: this.categorySelectItems[0].id,
+			text: '',
+		});
+	}
+	removeQuestion(row: number) {
+		this.addingQuestionsData.splice(row, 1);
+	}
+	
+	// -----------------------------------------------------
+
+	callbackSetQuestionType(entry: AddQuestionEntry, value: boolean): void {
+		entry.isAccount = value;
+	}
+	
+	callbackSelectTranche(entry: AddQuestionEntry) {
+		entry.accountId = null;
 	}
 }
