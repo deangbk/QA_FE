4c48cfdb94469bb820df87b357e00f359019745d
Natashi <32347635+Natashi@users.noreply.github.com>
2024-04-17 13:53:25 +0700

    Remove .spec.ts

--- src/app/Pipes/q-approve.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { QApprovePipe } from './q-approve.pipe';
-
-describe('QApprovePipe', () => {
-  it('create an instance', () => {
-     // @ts-ignore
-    const pipe = new QApprovePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/Pipes/search-basic.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchBasicPipe } from './search-basic.pipe';
-
-describe('SearchBasicPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchBasicPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
