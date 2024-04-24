43d050c5fb4da6ddc3dc31848b5b125d36c6f008
Natashi <32347635+Natashi@users.noreply.github.com>
2024-04-24 16:25:39 +0700

    Add option to create new tranche

--- src/app/admin-section/manage-project/project-edit-tranches/project-edit-tranches.component.ts
+++ src/app/admin-section/manage-project/project-edit-tranches/project-edit-tranches.component.ts
@@ -14,7 +14,8 @@ import * as Models from 'app/data/data-models';
 import { Helpers } from 'app/helpers';
 
 import { ModalLine, ConfirmDeleteModalComponent } from 'app/modals/confirm-delete-modal/confirm-delete-modal.component';
-import { TrancheEditModalComponent } from 'app/modals/tranche-edit-modal/tranche-edit-modal.component';
+import { TrancheEditModalComponent, FnCheckDuplicate } from 'app/modals/tranche-edit-modal/tranche-edit-modal.component';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'project-edit-tranches',
@@ -41,6 +42,24 @@ export class ProjectEditTranchesComponent implements OnInit {
 	
 	// -----------------------------------------------------
 	
+	checkDuplicatedTranche: FnCheckDuplicate = (name: string) => {
+		return this.tranches.findIndex(x => x.name == name) != -1;
+	};
+	
+	// -----------------------------------------------------
+	
+	sendObservableAndResult<T>(obs: Observable<T>, successMsg: string) {
+		obs.subscribe({
+			next: x => {
+				this.notifier.notify('success', successMsg);
+				this.onrefresh.emit();
+			},
+			error: x => {
+				this.notifier.notify('error', 'Server Error: ' + Helpers.formatHttpError(x));
+			},
+		});
+	}
+	
 	callbackEdit(tranche: Models.RespTrancheDataEx) {
 		console.log(tranche.id);
 		
@@ -48,21 +67,34 @@ export class ProjectEditTranchesComponent implements OnInit {
 		const inst = modalRef.componentInstance as TrancheEditModalComponent;
 		{
 			inst.tranche = tranche;
+			inst.fnCheckDuplicate = this.checkDuplicatedTranche;
 		}
 		
 		inst.result.subscribe((result) => {
 			if (result.some) {
-				let edit = result.val;
+				let edit = result.val as Models.ReqBodyEditTranche;
+				if (edit.name != tranche.name) {
+					let obs = this.dataService.trancheEdit(tranche.id, edit);
+					this.sendObservableAndResult(obs, "Tranche edited");
+				}
+			}
+		});
+	}
+	
+	callbackCreate() {
+		const modalRef = this.modalService.open(TrancheEditModalComponent);
+		const inst = modalRef.componentInstance as TrancheEditModalComponent;
+		{
+			inst.creating = true;
+			inst.fnCheckDuplicate = this.checkDuplicatedTranche;
+		}
+
+		inst.result.subscribe((result) => {
+			if (result.some) {
+				let create = result.val as Models.ReqBodyCreateTranche;
 				
-				this.dataService.trancheEdit(tranche.id, edit).subscribe({
-					next: x => {
-						this.notifier.notify('success', `Tranche edited`);
-						this.onrefresh.emit();
-					},
-					error: x => {
-						this.notifier.notify('error', 'Server Error: ' + Helpers.formatHttpError(x));
-					},
-				});
+				let obs = this.dataService.trancheCreate(create);
+				this.sendObservableAndResult(obs, "Tranche created!");
 			}
 		});
 	}
@@ -84,15 +116,8 @@ export class ProjectEditTranchesComponent implements OnInit {
 		
 		inst.result.subscribe((result: boolean) => {
 			if (result) {
-				this.dataService.trancheDelete(tranche.id).subscribe({
-					next: x => {
-						this.notifier.notify('success', `Tranche deleted`);
-						this.onrefresh.emit();
-					},
-					error: x => {
-						this.notifier.notify('error', 'Server Error: ' + Helpers.formatHttpError(x));
-					},
-				});
+				let obs = this.dataService.trancheDelete(tranche.id);
+				this.sendObservableAndResult(obs, "Tranche deleted");
 			}
 		});
 	}
--- src/app/modals/tranche-edit-modal/tranche-edit-modal.component.ts
+++ src/app/modals/tranche-edit-modal/tranche-edit-modal.component.ts
@@ -11,6 +11,9 @@ import { Option, Some, None } from 'ts-results';
 import * as Models from 'app/data/data-models';
 import { Helpers } from 'app/helpers';
 
+type ModelCreateOrEdit = Models.ReqBodyEditTranche | Models.ReqBodyCreateTranche;
+export type FnCheckDuplicate = (name: string) => boolean;
+
 @Component({
 	selector: 'tranche-edit-modal',
 	templateUrl: './tranche-edit-modal.component.html',
@@ -18,7 +21,10 @@ import { Helpers } from 'app/helpers';
 })
 export class TrancheEditModalComponent implements OnInit {
 	@Input() public tranche: Models.RespTrancheData = null;
-	@Output() public result: EventEmitter<Option<Models.ReqBodyEditTranche>>
+	@Input() public fnCheckDuplicate: FnCheckDuplicate = null;
+	@Input() public creating = false;
+	
+	@Output() public result: EventEmitter<Option<ModelCreateOrEdit>>
 		= new EventEmitter();
 	
 	name: string;
@@ -31,14 +37,34 @@ export class TrancheEditModalComponent implements OnInit {
 		this.name = this.tranche.name;
 	}
 	
+	getNameError(): string | null {
+		if (this.name == null || this.name.length == 0)
+			return "Name must not be empty";
+		if (this.name.length > 16)
+			return "Name must not be longer than 16 characters";
+		if (this.tranche == null || this.name != this.tranche.name) {
+			if (this.fnCheckDuplicate != null && this.fnCheckDuplicate(this.name))
+				return "Name must be unique";
+		}
+		return null;
+	}
+	
 	// -----------------------------------------------------
 	
 	callbackAction(confirm: boolean) {
 		if (confirm) {
-			let edit: Models.ReqBodyEditTranche = {
-				name: this.name,
-			};
-			this.result.emit(Some(edit));
+			let model: ModelCreateOrEdit;
+			if (this.creating) {
+				model = <Models.ReqBodyCreateTranche>{
+					name: this.name,
+				};
+			}
+			else {
+				model = <Models.ReqBodyEditTranche>{
+					name: this.name,
+				};
+			}
+			this.result.emit(Some(model));
 		}
 		else {
 			this.result.emit(None);
