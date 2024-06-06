e4a252a6466ff6a11a89103b773bc61b90b18a6a
Natashi <32347635+Natashi@users.noreply.github.com>
2024-06-06 14:48:14 +0700

    Fix AdminSecurityService not working on Chromium

--- src/app/admin/service/security.service.ts
+++ src/app/admin/service/security.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-
 import { DataService } from 'app/data/data.service';
 import * as Models from 'app/data/data-models';
 
 import { SecurityService } from 'app/security/security.service';
 
 import { Helpers } from 'app/helpers';
+
+// Identical to ProjectSecurityService except for storage key names
+
+const KEY_TOKEN = 'TOKEN2';
+const KEY_EXPIRE = 'EXP2';
 
 @Injectable()
 export class AdminSecurityService extends SecurityService {
@@ -20,25 +23,23 @@ export class AdminSecurityService extends SecurityService {
 	expire: string;
 	
 	override getToken(): string {
-		return this.token ?? '';
+		const value = localStorage.getItem(KEY_TOKEN);
+		return value ?? '';
 	}
 	override getExpiration(): string {
-		return this.expire ?? '';
+		const value = localStorage.getItem(KEY_EXPIRE);
+		return value ?? '';
 	}
 	
 	override storeLoginToken(authRes: any): void {
 		this.removeLoginToken();
-		
+
 		const authAsModel = authRes as Models.RespLoginToken;
-		this.token = authAsModel.token;
-		this.expire = authAsModel.expiration;
+		localStorage.setItem(KEY_TOKEN, authAsModel.token);
+		localStorage.setItem(KEY_EXPIRE, authAsModel.expiration);
 	}
 	override removeLoginToken(): void {
 		this.tokenData = null;
-		
-		this.token = null;
-		this.expire = null;
-	}
 
 		localStorage.removeItem(KEY_TOKEN);
 		localStorage.removeItem(KEY_EXPIRE);
